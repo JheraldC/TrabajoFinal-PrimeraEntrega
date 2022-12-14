20937cbb9865324cd2a0339d255a73d430404e7c
JheraldC <jcaceresap@unsa.edu.pe>
2022-12-14 19:15:37 +0000

    imprime el html inicial de la pagina

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use CGI;
+use DBI;
 
+print "Content-type: text/html; charset=UTF-8\n\n";
+print<<HTML;
+<!DOCTYPE html>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <title>Listado</title>
+  </head>
+  <body>
+  <h1>Nuestras páginas wiki</h1>
+  <hr>
+HTML
