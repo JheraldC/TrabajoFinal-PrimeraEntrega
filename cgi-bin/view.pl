ff12c2de6b0e3a26ebead6061f0b543a74e2bfbe
JheraldC <jcaceresap@unsa.edu.pe>
2022-12-14 07:10:52 +0000

    imprimiendo html

--- cgi-bin/view.pl
+++ cgi-bin/view.pl
@@ -8,3 +8,13 @@ my $q = CGI->new;
 my $title = $q->param('title');
 print $q->header('text/html; charset=utf-8');
 
+print <<HTML;
+<!DOCTYPE html>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<html>
+  <head>
+  <title>Ver</title>
+  </head>
+HTML
+
+
