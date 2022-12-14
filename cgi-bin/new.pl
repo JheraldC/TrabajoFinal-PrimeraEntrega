836159518ac7ad903462972fa4f16e2692cef343
JheraldC <jcaceresap@unsa.edu.pe>
2022-12-14 19:40:12 +0000

    añadiendo parametros cgi y parametros markdown con title

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -1,4 +1,14 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use CGI;
+use DBI;
+
+my $q = CGI->new;
+print $q->header("text/html; charset=utf-8");
+my $titulo = $q->param('titulo');
+my $markdown = $q->param('markdown');
+#Segundo
+my $title = $q->param('title';
+my $markdown2 = $q->param('markdown2');
 
