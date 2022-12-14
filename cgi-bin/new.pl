b3ed1663ef03bb15b83f733948cbe42513c6d3aa
JheraldC <jcaceresap@unsa.edu.pe>
2022-12-14 19:44:07 +0000

    agregando el insert en la base de datos e imprimiendo el html con titulo y markdown

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -17,3 +17,26 @@ my $password = 'pweb1';
 my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
+if((defined($titulo) and defined($markdown)) or (!defined($title) and !defined($markdown2))){
+my $sth = $dbh->prepare("INSERT INTO Fakewiki(Title, Text) VALUES (?,?)");
+$sth->execute($titulo, $markdown);
+
+print <<BLOCK;
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Pagina wiki</title>
+  </head>
+    <body>
+    <h1>$titulo</h1>
+    <br>
+    <h3>$markdown</h3>
+    <hr>
+    <h2>Página grabada
+    <a href="list.pl"> Lista de páginas</a>
+    </h2>
+    </body>
+</html>
+BLOCK
+}
+
