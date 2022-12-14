c0f4df59c919021881a1ca7bbfc0da2044bc5ba8
JheraldC <jcaceresap@unsa.edu.pe>
2022-12-14 19:29:30 +0000

    agregando la base de datos

--- cgi-bin/edit.pl
+++ cgi-bin/edit.pl
@@ -8,3 +8,14 @@ my $q = CGI->new;
 my $title = $q->param('title');
 print $q->header('text/html; charset=utf-8');
 
+print "<h1>$title</h1>"
+
+my @registro;
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+my $sth = $dbh->prepare("SELECT Text FROM Fakewiki WHERE Title=?");
+$sth->execute($title);
+
